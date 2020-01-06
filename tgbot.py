5fe1ce7335eed140d05903cb471aea7c5cf34aa5
Lukxsx <luks@luks.moe>
2020-01-06 18:31:10 +0200

    logging to terminal

--- tgbot.py
+++ tgbot.py
@@ -1,6 +1,5 @@
-import telegram, sys, random, logging
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
+import telegram, sys, random
+from telegram.ext import Updater, CommandHandler
 from twitter_scraper import get_tweets
 
 bot_token = sys.argv[1]
@@ -16,19 +15,10 @@ bot = telegram.Bot(token=bot_token)
 updater = Updater(token=bot_token, use_context=True)
 dispatcher = updater.dispatcher
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
-
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="asd")
-
 def tweet(update, context):
 	tweet = tweets[random.randint(0, len(tweets))]
 	context.bot.send_message(chat_id=update.effective_chat.id, text=tweet)
-
-
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+	print(tweet)
 
 tweet_handler = CommandHandler('tweet', tweet)
 dispatcher.add_handler(tweet_handler)
